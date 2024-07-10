d03fb4e9b47e0b3b003c4cebfa82fe66e1240205
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-07-10 12:30:15 -0700

    add border radius functionality

--- src/components/Box/Hero/HeroBox.jsx
+++ src/components/Box/Hero/HeroBox.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HeroBoxStyles.scss";
 const HeroBox = () => {
+  const [radius, setRadius] = useState(20);
+
+  const boxStyle = {
+    borderRadius: `${radius}px`,
+  };
+
+  const onRadiusChange = (e) => {
+    setRadius(e.target.value);
+  };
+
   return (
     <div className="hero-box-container">
-      <div className="hero-box"></div>
+      <div className="hero-box" style={boxStyle}></div>
+      <div className="hero-box-radius-container">
+        <span>border-radius</span>
+        <input
+          type="range"
+          min="0"
+          max="200"
+          className="hero-radius slider"
+          value={radius}
+          onChange={onRadiusChange}
+        />
+        {/* {radius}px */}
+        <input
+          className="hero-radius"
+          type="number"
+          min="0"
+          max="200"
+          value={radius}
+          onChange={onRadiusChange}
+        />
+      </div>
     </div>
   );
 };
