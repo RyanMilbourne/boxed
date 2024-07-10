6d586ed6c954279d9abf86c0de24b93959f71be5
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-07-10 14:01:55 -0700

    add aspect ratio functionality

--- src/components/Box/Hero/HeroBox.jsx
+++ src/components/Box/Hero/HeroBox.jsx
@@ -3,13 +3,19 @@ import "./HeroBoxStyles.scss";
 import RoundedCornerIcon from "@mui/icons-material/RoundedCorner";
 const HeroBox = () => {
   const [radius, setRadius] = useState(20);
+  const [aspectRatio, setAspectRatio] = useState("1/1");
 
   const boxStyle = {
     borderRadius: `${radius}px`,
+    aspectRatio: aspectRatio,
   };
 
   const onRadiusChange = (e) => {
     setRadius(e.target.value);
+  };
+
+  const onRatioChange = (e) => {
+    setAspectRatio(e.target.textContent);
   };
 
   return (
@@ -34,6 +40,17 @@ const HeroBox = () => {
           value={radius}
           onChange={onRadiusChange}
         />
+      </div>
+      <div className="hero-box-aspect-ratio-container">
+        {["1/1", "3/4", "4/3", "4/5", "5/4", "16/9"].map((ratio) => (
+          <div
+            key={ratio}
+            className={`aspect-ratio ${aspectRatio === ratio ? "active" : ""}`}
+            onClick={onRatioChange}
+          >
+            {ratio}
+          </div>
+        ))}
       </div>
     </div>
   );
