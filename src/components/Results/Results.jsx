15b31444dffdfea74c0f04f0f344b7ae6cafe2cd
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-07-11 13:48:59 -0700

    add copy to clipboard functionallity

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,22 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { ControlsContext } from "../../hooks/ControlsContext";
 import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
+import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
 import "./ResultsStyles.scss";
 
 const Results = () => {
-  const { boxShadows, addBoxShadow } = useContext(ControlsContext);
+  const { boxShadows } = useContext(ControlsContext);
   const boxShadowValue = boxShadows
     .map(
       (shadow) =>
         `${shadow.xPosition}px ${shadow.yPosition}px ${shadow.blurValue}px ${shadow.spreadValue}px ${shadow.color}`
     )
     .join(", ");
+
+  const [copySuccess, setCopySuccess] = useState(false);
+  const textRef = useRef(null);
+
+  const copyToClipboard = async (e) => {
+    try {
+      await navigator.clipboard.writeText(`box-shadow: ${boxShadowValue}`);
+      setCopySuccess(true);
+      setTimeout(() => setCopySuccess(false), 1000);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
+
+  const iconStyle = {
+    width: "1.5rem",
+    height: "1.5rem",
+    margin: "0",
+    padding: "0",
+  };
   return (
     <div className="results-container">
-      <div className="copy-to-clipboard-wrapper">
-        <ContentCopyRoundedIcon />
+      <div
+        className={
+          !copySuccess
+            ? "copy-to-clipboard-wrapper"
+            : "copy-to-clipboard-wrapper success"
+        }
+        onClick={copyToClipboard}
+      >
+        {copySuccess && <CheckCircleOutlineRoundedIcon style={iconStyle} />}
+        {!copySuccess && <ContentCopyRoundedIcon style={iconStyle} />}
       </div>
-      <div className="results-text">box-shadow: {boxShadowValue}</div>
+      <div className="results-text" ref={textRef}>
+        box-shadow: {boxShadowValue}
+      </div>
     </div>
   );
 };
