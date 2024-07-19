3c51401798c51e6e003f9c48622e15f945ace475
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-07-19 14:55:29 -0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import { useState } from "react";
 import "./App.css";
 import BoxHome from "./components/Box/BoxHome";
 import Results from "./components/Results/Results";
+import Navbar from "./components/Nav/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="app-container">
-      <div className="app-wrapper">
-        <div className="app-title">
+    <>
+      <Navbar />
+      <div className="app-container">
+        <div className="app-wrapper">
+          {/* <div className="app-title">
           <h1>boxed</h1>
         </div>
         <div className="app-description">
           <p>a playful way to visualize box-shadows</p>
+        </div> */}
+          <BoxHome />
+          <Results />
         </div>
-        <BoxHome />
-        <Results />
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Nav/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NavStyles.scss";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo-wrapper">boxed.</div>
+      <div className="nav-links-wrapper"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
