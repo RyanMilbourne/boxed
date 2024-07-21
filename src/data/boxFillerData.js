018b56870f2e41ad4df58a2ee4e932fa46b76b1b
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-07-21 15:29:06 -0700

    update default text

--- src/data/boxFillerData.js
+++ src/data/boxFillerData.js
@@ -1,5 +1,5 @@
 const boxFillerData = {
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Nulla facilisi. Pellentesque facilisis, lorem vel fermentum elementum, eros sapien scelerisque lacus.",
+  text: "Add text!",
   image: "https://tinyurl.com/575xpe93",
 };
 
